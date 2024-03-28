9735c88fa8b282f8827d8d678fc66ad3f6ff6cf6
Janki Thakkar <jankithakkar2003@gmail.com>
2024-03-28 23:42:24 +0530

    SignIn to backend attachment incomplete

--- frontend/src/components/SignIn/Password.js
+++ frontend/src/components/SignIn/Password.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './SignIn.css'
 
-export default function Password() {
+export default function Password({user, setUser}) {
+
+  const handlePasswordChange = (e) => {
+    setUser({...user,password:e.target.value});
+  }
+
   return (
     <div className='form-group field1'>
         <button className='btn icon1'><i className='fa fa-lock'></i></button>
-        <input type='password' id='inputPassword' className='form-control' placeholder='Password' required/>
+        <input type='password' id='inputPassword' className='form-control' placeholder='Password' value={user.password} onChange={handlePasswordChange} required/>
     </div>
   )
 }
--- frontend/src/components/SignIn/SignInForm.js
+++ frontend/src/components/SignIn/SignInForm.js
@@ -1,22 +1,66 @@
-import React from 'react'
-import Password from './Password'
-import './SignIn.css'
-import SignUpBtn from './SignUpBtn'
-import { Link } from 'react-router-dom'
-import Username from './Username'
-import SignInBtn from './SignInBtn'
+import React from "react";
+import Password from "./Password";
+import "./SignIn.css";
+import SignUpBtn from "./SignUpBtn";
+import { Link } from "react-router-dom";
+import Username from "./Username";
+import SignInBtn from "./SignInBtn";
+import { useState } from "react";
+
+const USER_AUTH = "http://localhost:3000/api/v1/auth/authenticate";
 
 export default function SignInForm() {
+
+  const [user, setUser] = useState({ username: "", password: "" });
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(user);
+  
+    fetch(USER_AUTH,{
+      method:"POST",
+      headers: {
+        accept: 'application/json',
+        "Content-Type": "application/json",
+      },
+      credentials:"include",
+      body:JSON.stringify(user)
+    }).then((res) => {
+      // Check if response is successful
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      // Parse the JSON response
+      return res.json();
+    }).then((res)=>{
+      console.log(res,"---------------");
+      if(res.success){
+        localStorage.setItem("user",JSON.stringify(res.data));
+      }
+      // console.log();
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
   return (
-    <form className='signin1'>
-        <div className='form-parent'>
-            <Username/>
-            <Password/>
-            <SignInBtn/>
-            <div className='callout'>
-                <span className='span1'>Don't have account? <Link to='/sign-up'>Create Account</Link></span>
-            </div>
+    <form className="signin1">
+      <div className="form-parent">
+        <Username user={user} setUser={setUser} />
+        <Password user={user} setUser={setUser} />
+        <div>
+          <button type="submit" className="signinbtn1" formAction="" onClick={handleSubmit}>
+            Sign In
+          </button>
         </div>
+        <div className="callout">
+          <span className="span1">
+            Don't have account? <Link to="/sign-up">Create Account</Link>
+          </span>
+        </div>
+      </div>
     </form>
-  )
+  );
 }
