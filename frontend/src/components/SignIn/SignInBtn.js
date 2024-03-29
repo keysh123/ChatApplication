1d338ec05bfbca784564f9a404928faf2edb3d6a
JankiThakkar7 <144004849+JankiThakkar7@users.noreply.github.com>
2024-03-29 17:43:03 +0530

    Add files via upload

---
+++ frontend/src/components/SignIn/SignInBtn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SignIn.css'
+
+export default function SignInBtn() {
+  return (
+    <div>
+        <button type="submit" className="signinbtn1" formAction=''>Sign In</button>
+    </div>
+  )
+}
