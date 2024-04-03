6c1289b40b13c5a6852a338e6206722477944e58
Mayuri473 <mayuriraval473@gmail.com>
2024-04-03 11:05:11 +0530

    add close btn

--- frontend/src/components/UserInfo/UserInfo.js
+++ frontend/src/components/UserInfo/UserInfo.js
@@ -2,15 +2,16 @@ import React from 'react'
 import './UserInfoPage.css'
 import UserImg from './UserImg'
 import UserData from './UserData'
+import { Link } from 'react-router-dom'
 
 const UserInfo = () => {
   return (
     <>
       <div className='userInfoPage container-fluid'>
-        <div className="row profile">Profile</div>
+        <div className="row profile"> <span className="col-11 prof">Profile</span> <Link to="/chat-page" className='close col-1'> <span ><i className="fa-solid fa-circle-xmark"></i></span> </Link> </div>
         <div className="row userfield">
           <div className="col-5"> <UserImg /> </div>
-          <div className="col-7"> <UserData/> </div>
+          <div className="col-7"> <UserData /> </div>
         </div>
       </div>
     </>
