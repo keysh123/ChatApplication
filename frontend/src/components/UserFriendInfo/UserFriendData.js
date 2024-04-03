fb3000cc24bfe7a793dbb8a000b6658509652d7e
Janki Thakkar <jankithakkar2003@gmail.com>
2024-04-03 09:14:04 +0530

    Disable input in UserFriendData

--- frontend/src/components/UserFriendInfo/UserFriendData.js
+++ frontend/src/components/UserFriendInfo/UserFriendData.js
@@ -7,9 +7,9 @@ const UserFriendData = () => {
             <div className='userdatabox'>
                 <form action="" className='uiform'>
                     <div className="uridiv">
-                        <div className='uiuname uir'>User Name : <input type='text'></input></div>
+                        <div className='uiuname uir'>User Name : <input type='text' disabled></input></div>
                         <div className='uimail uir'>Mail : <input type='email' disabled></input></div>
-                        <div className='uiabout uir'>About : <input type='text'></input></div>
+                        <div className='uiabout uir'>About : <input type='text' disabled></input></div>
                     </div>
                 </form>
             </div>
