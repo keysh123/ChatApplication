ce52199167ee5de918c87ff9aecd9d80c47fed56
Janki Thakkar <jankithakkar2003@gmail.com>
2024-04-04 09:26:32 +0530

    No changes

--- frontend/src/components/ChatPage/Input.jsx
+++ frontend/src/components/ChatPage/Input.jsx
@@ -1,36 +1,40 @@
-import React, { useState } from 'react'
-import Attach from '../img/attach.png'
-import Img from '../img/img.png'
-import './ChatPage.css'
+import React, { useState } from "react";
+import Attach from "../img/attach.png";
+import Img from "../img/img.png";
+import "./ChatPage.css";
 
 const Input = () => {
   const [text, setText] = useState("");
 
   const handleSendBtn = (e) => {
+    const trimmedText = e.target.value.trim();
     setText(e.target.value);
     const send = document.querySelector(".sendbtn");
-    if (e.target.value !== "") {
+    if (trimmedText !== "" && trimmedText !== null) {
       send.style.backgroundColor = "blue";
-    }else{
+    } else {
       send.style.backgroundColor = "#8da4f1";
     }
-  }
+  };
 
   return (
-
-    <div className='input'>
-      <input onChange={handleSendBtn} type="text" placeholder='Type something' value={text} />
+    <div className="input">
+      <input
+        onChange={handleSendBtn}
+        type="text"
+        placeholder="Type something"
+        value={text}
+      />
       <div className="send">
         <img src={Img} alt="" />
         <input type="file" name="" id="file" style={{ display: "none" }} />
         <label htmlFor="file">
           <img src={Attach} alt="" />
         </label>
-        <button className='sendbtn'>Send</button>
+        <button className="sendbtn">Send</button>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Input
+export default Input;
